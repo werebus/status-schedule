6e7a094f742e2f936faf83373d458dc625f5882a
Matt Moretti <werebus@gmail.com>
2017-11-22 14:39:43 -0500

    Added `reload!` to console.

--- lib/console.rb
+++ lib/console.rb
@@ -1,9 +1,19 @@
 require 'bundler'
 Bundler.require
 
-config_file = File.join(File.dirname(__FILE__), '..', 'config', 'application.yml')
+unless defined? PROJECT_ROOT
+  PROJECT_ROOT = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+end
+
+config_file = File.join(PROJECT_ROOT, 'config', 'application.yml')
 app = Figaro::Application.new(path: config_file)
 app.load
 Figaro.application = app
+
+def reload!
+  $".select{|path| path.start_with? PROJECT_ROOT}.each do |path|
+    load path
+  end
+end
 
 require 'status_changer'
