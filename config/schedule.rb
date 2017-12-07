67394eaebe2b1ee301256a8718a4f6ffebc83a4e
Matt Moretti <werebus@gmail.com>
2017-12-07 11:48:23 -0500

    Rubocup steered me wrong.
    Sortof. It's Kernel#format, not String#format. This is in preference to
    String#%, I guess.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,13 +1,13 @@
 rake_template = 'cd :path && bundle exec rake %<task>s --silent :output'
 
 job_type :rake,
-         rake_template.format(task: ':task')
+         format(rake_template, { task: ':task' })
 job_type :change,
-         rake_template.format(task: '"status:change[:from, :text, :emoji]"')
+         format(rake_template, { task: '"status:change[:from, :text, :emoji]"' })
 job_type :clear,
-         rake_template.format(task: 'status:clear')
+         format(rake_template, { task: 'status:clear' })
 job_type :set,
-         rake_template.format(task: '"status:set[:text, :emoji]"')
+         format(rake_template, { task: '"status:set[:text, :emoji]"' })
 
 #every :day, at: '8:00' do
 #  change :status, from: '', text: 'At Transit', emoji: 'oncoming_bus'
